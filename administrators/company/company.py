ed51d99986d5e1c2389724985a89195b60eefc14
huangshichen1 <1641452570@qq.com>
2018-02-24 15:06:15 +0800

    开始写公司后台管理
    删除了后台的公司类，等会合并分析分支，使用分析的公司类

--- administrators/company/company.py
+++ administrators/company/company.py
@@ -2,34 +2,97 @@ from django.db import models
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
+import administrators.models as m
+import  pandas
 
-# 公司类
-class Company(models.Model):
-    # 公司id
-    companyId = models.IntegerField()
-    # 公司名称
-    compName = models.CharField(max_length=50)
-    # 公司规模
-    compSize = models.CharField(max_length=20)
-    # 公司行业
-    compIndustry = models.CharField(max_length=50)
-    # 公司标签
-    companyLabels = models.CharField(max_length=50)
-    # 公司链接
-    compLink = models.CharField(max_length=200)
-    # 公司介绍
-    compIntroduce = models.TextField()
-    # 联系信息
-    contactInfo = models.CharField(max_length=50)
-    # 经度
-    longitude = models.FloatField()
-    # 纬度
-    latitude = models.FloatField()
-    # 商业区域
-    businessZones = models.CharField(max_length=50)
-    # 公司主页
-    compHome = models.CharField(max_length=200)
-    # 公司LOGO
-    companyLogo = models.CharField(max_length=100)
-    # 融资
-    financeStage = models.CharField(max_length=20)
+# 展示公司
+def show(request):
+    coms = m.Company.objects.all()
+    return render(request, 'company/companyShow.html', {'coms': coms})
+
+# 删除岗位
+def delete(request):
+    jid = request.GET['jid']
+    print('jid', 'xxxxxxxx')
+    m.Job.objects.get(id=jid).delete()
+    return show(request)
+
+# 跳转到添加岗位界面
+def add(request):
+    return  render(request, 'job/jobAdd.html')
+
+# 存储岗位
+def save(request):
+    JobName = request.POST['JobName']
+    JobPlace = request.POST['JobPlace']
+    JobSalary = request.POST['JobSalary']
+    JobAdvantage = request.POST['JobAdvantage']
+    releaseTime = request.POST['releaseTime']
+    jobNeed = request.POST['jobNeed']
+    educationRequire = request.POST['educationRequire']
+    experienceRequire = request.POST['experienceRequire']
+    skillRequire = request.POST['skillRequire']
+    jobLink = request.POST['jobLink']
+    jobInfo = request.POST['jobInfo']
+    jobNature = request.POST['jobNature']
+    jobLabels = request.POST['jobLabels']
+
+    job = m.Job(JobName=JobName, JobPlace=JobPlace,
+             JobSalary=JobSalary,JobAdvantage=JobAdvantage,
+             releaseTime=releaseTime,jobNeed=jobNeed,
+             educationRequire=educationRequire,experienceRequire=experienceRequire,
+             skillRequire=skillRequire,jobLink=jobLink,jobInfo=jobInfo,
+             jobNature=jobNature,jobLabels=jobLabels)
+    job.save()
+    return show(request)
+
+# 跳转到编辑岗位界面
+def edit(request):
+    jid = request.GET['jid']
+    job = m.Job.objects.get(id=jid)
+    return render(request, 'job/jobEditor.html', {'job' : job})
+
+# 更新岗位
+def update(request):
+    jid = request.POST['jid']
+    JobName = request.POST['JobName']
+    JobPlace = request.POST['JobPlace']
+    JobSalary = request.POST['JobSalary']
+    JobAdvantage = request.POST['JobAdvantage']
+    releaseTime = request.POST['releaseTime']
+    jobNeed = request.POST['jobNeed']
+    educationRequire = request.POST['educationRequire']
+    experienceRequire = request.POST['experienceRequire']
+    skillRequire = request.POST['skillRequire']
+    jobLink = request.POST['jobLink']
+    jobInfo = request.POST['jobInfo']
+    jobNature = request.POST['jobNature']
+    jobLabels = request.POST['jobLabels']
+
+    job = m.Job.objects.get(id=jid)
+    job.JobName = JobName
+    job.JobPlace = JobPlace
+    # job.JobSalary = JobSalary
+    job.JobAdvantage = JobAdvantage
+    job.releaseTime = releaseTime
+    job.jobNeed = jobNeed
+    job.educationRequire = educationRequire
+    job.experienceRequire = experienceRequire
+    job.skillRequire = skillRequire
+    job.jobLink = jobLink
+    job.jobInfo = jobInfo
+    job.jobNature = jobNature
+    job.jobLabels = jobLabels
+
+    job.save()
+    return show(request)
+
+def readDate():
+    pa = pandas.read_csv(r'D:/java_data.csv',encoding='gbk', header=None,names=['JobName',
+        'JobPlace', 'JobSalary', 'JobAdvantage', 'releaseTime', 'jobNeed',
+        'educationRequire', 'experienceRequire', 'skillRequire', 'jobLink',
+        'jobInfo', 'jobNature', 'jobLabels'])
+    # pa = pandas.read_csv(r'D:/data.txt', header=None, names=['a', 'b', 'c'])
+    print(pa.head(5))
+
+
---
+++ administrators/user/userInfo.py
@@ -0,0 +1,5 @@
+from django.db import models
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+import administrators.models as m
